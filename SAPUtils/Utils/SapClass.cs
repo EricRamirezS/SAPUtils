b62f7c4f3024074276177e0730f01c72b57deb2e
Eric Ramirez <eramirezsantis@gmail.com>
2025-08-07 10:32:23 -0400

    Add `SapClass` utility in `SAPUtils` module
    - Introduced a new `SapClass` to encapsulate static references to `Company`, `Application`, `MainApplication`, and `Logger`.
    - Updated the `SAPUtils.csproj` file to include the new `SapClass.cs` file for compilation.

---
+++ SAPUtils/Utils/SapClass.cs
@@ -0,0 +1,11 @@
+﻿using SAPbouiCOM;
+using Company = SAPbobsCOM.Company;
+
+namespace SAPUtils.Utils {
+    public class SapClass {
+        public static Company Company => SapAddon.Instance().Company;
+        public static Application Application => SapAddon.Instance().Application;
+        public static SAPbouiCOM.Framework.Application MainApplication => SapAddon.Instance().MainApplication;
+        public static ILogger Log => SapAddon.Instance().Logger;
+    }
+}
