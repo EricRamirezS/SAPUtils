200c7a4c234cd8800ba86e1f814b82cc4b486036
Eric Ramirez <eramirezsantis@gmail.com>
2025-09-06 00:23:38 -0400

    Enhance reflection handling for `UserForm` properties
    - Introduced static initialization for `FormResource` and `Initialized` using compiled expression trees for improved performance.
    - Replaced direct reflection calls with cached delegates (`GetFormResource` and `SetInitialized`) to optimize property access.
    - Streamlined `InitializedSetter` and `FormResource` getter implementations to utilize the new cached expressions.

--- SAPUtils/Forms/UserForm.Reflections.cs
+++ SAPUtils/Forms/UserForm.Reflections.cs
@@ -1,33 +1,38 @@
-﻿using System.Reflection;
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
 using SAPbouiCOM.Framework;
 
 namespace SAPUtils.Forms {
     public abstract partial class UserForm {
-        private string FormResource
-        {
-            get
-            {
-                PropertyInfo propertyInfo =
-                    typeof(FormBase).GetProperty("FormResource", BindingFlags.NonPublic | BindingFlags.Instance);
-                string formResource = "";
-                if (propertyInfo != null) {
-                    formResource = (string)propertyInfo.GetValue(this);
-                }
+        private static readonly Func<FormBase, string> GetFormResource;
+        private static readonly Action<FormBase, bool> SetInitialized;
 
-                return formResource;
+        [Localizable(false)]
+        static UserForm() {
+            PropertyInfo formResourceProp = typeof(FormBase).GetProperty("FormResource", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (formResourceProp != null) {
+                ParameterExpression instance = Expression.Parameter(typeof(FormBase), "instance");
+                MemberExpression property = Expression.Property(instance, formResourceProp);
+                GetFormResource = Expression.Lambda<Func<FormBase, string>>(property, instance).Compile();
+            }
+
+            PropertyInfo initializedProp = typeof(FormBase).GetProperty("Initialized", BindingFlags.NonPublic | BindingFlags.Instance);
+            // ReSharper disable once InvertIf
+            if (initializedProp != null) {
+                ParameterExpression instance = Expression.Parameter(typeof(FormBase), "instance");
+                ParameterExpression value = Expression.Parameter(typeof(bool), "value");
+                BinaryExpression body = Expression.Assign(Expression.Property(instance, initializedProp), value);
+                SetInitialized = Expression.Lambda<Action<FormBase, bool>>(body, instance, value).Compile();
             }
         }
 
+        private string FormResource => GetFormResource?.Invoke(this);
+
         internal bool InitializedSetter
         {
-            set
-            {
-                PropertyInfo propertyInfo =
-                    typeof(FormBase).GetProperty("Initialized", BindingFlags.NonPublic | BindingFlags.Instance);
-                if (propertyInfo != null) {
-                    propertyInfo.SetValue(this, value);
-                }
-            }
+            set => SetInitialized?.Invoke(this, value);
         }
     }
 }
